f709222e08642d025fc76236fed68f533822307d
Jasna Stankovic <jasna.stankovic@pumble.com>
2023-03-24 10:39:33 +0100

    Remove sample pdf from minify process

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1054,10 +1054,6 @@ function buildGeneric(defines, dir) {
         ])
       )
       .pipe(gulp.dest(dir + "web")),
-
-    gulp
-      .src("web/compressed.tracemonkey-pldi-09.pdf")
-      .pipe(gulp.dest(dir + "web")),
   ]);
 }
 
