04965906972da26ad2668abde5773bf85ebd5c13
Jasna Stankovic <jasna.stankovic@cake.com>
2025-09-22 22:06:23 +0200

    Add comment

--- web/viewer.js
+++ web/viewer.js
@@ -33,6 +33,7 @@ const AppConstants =
 window.PDFViewerApplication = PDFViewerApplication;
 window.PDFViewerApplicationConstants = AppConstants;
 window.PDFViewerApplicationOptions = AppOptions;
+// expose viewer version
 window.PDFViewerApiVersion = pdfjsVersion;
 
 function getViewerConfiguration() {
